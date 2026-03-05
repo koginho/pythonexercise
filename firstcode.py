8ecaf14f9663b15ff3ff4b8fe32821e9b199b40e
koginho <mcatalanodraw@gmail.com>
2026-03-05 16:01:34 -0300

    [ADD] Código com entrada de valores e verificação de tipo.

---
+++ firstcode.py
@@ -0,0 +1,31 @@
+# Objetivo: criar uma função que recebe dois parametros (number, string)
+# a função deve virificar e exibir esses valores.
+
+# Recebe os valores do usuário
+minhaString = input('Digite um texto: ')
+meuNumero = int(input('Digite um número inteiro: '))
+
+def imprimir_parametros(texto, numero):
+    """
+    Função que imprime dois valores.
+
+    Esta função recebe dois valores, verifica se texto = str e se numero = int
+    e imprime o valor.
+
+    Args:
+        texto(string): valor atribuido pelo usuário.
+        numero(int): valor atribuido pelo usuário.
+    Returns:
+        None: sem retorno.
+    """
+    if texto.replace(" ", "").isalpha():
+        print(texto)
+    else:
+        print("texto inválido.")
+    if isinstance(numero, int):
+        print(numero)
+    else:
+        print("número inválido.")
+
+# Chamada da função
+imprimir_parametros(minhaString, meuNumero)
